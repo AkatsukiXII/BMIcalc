f883205145033a0c719afa320da92817907628c5
AkatsukiXII <942020ryan@gmail.com>
2023-10-02 19:39:26 +0800

    BMI calculator ver2.0

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ weight = int(input('Please insert your weight: '))
 
 bmi = weight / (height / 100) ** 2
 print('Your height: '+ str(height))
+print('Your weight: '+ str(weight))
+print('Your BMI: '+ str(round(bmi,2)))
+
